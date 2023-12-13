f9135c8a022727def2ce990af2d7fc473d7cd7c0
Nakano38 <149351695+Nakano38@users.noreply.github.com>
2023-12-13 16:11:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -17,40 +17,42 @@ mode = st.radio(
     "質問モードと回答モードを切り替えてお使いください",
     ["***出題***", "***回答***"],
     horizontal = True)
-"""
-@st.cache_resource(show_spinner=False)
-# チャットボットとやりとりする関数
-def load_data():
-    with st.spinner(text="しばらくお待ちください"):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="""
-        {テーマ} = 「安達としまむら」と「現代哲学」 
-        
-        あなたは{テーマ}の専門家です。質問に対して詳細な説明をしてください。
-        """))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-        return index
+
+if mode != "出題":
+  @st.cache_resource(show_spinner=False)
+  # チャットボットとやりとりする関数
+  def load_data():
+      with st.spinner(text="しばらくお待ちください"):
+          reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+          docs = reader.load_data()
+          service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="""
+          {テーマ} = 「安達としまむら」と「現代哲学」 
+          
+          あなたは{テーマ}の専門家です。質問に対して詳細な説明をしてください。
+          """))
+          index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+          return index
 
 
-index = load_data()
+  index = load_data()
 
-if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+  if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+          st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
 
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+  if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+      st.session_state.messages.append({"role": "user", "content": prompt})
 
-for message in st.session_state.messages: # Display the prior chat messages
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+  for message in st.session_state.messages: # Display the prior chat messages
+      with st.chat_message(message["role"]):
+          st.write(message["content"])
 
-# If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("考え中..."):
-            response = st.session_state.chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
-"""
+  # If last message is not from assistant, generate a new response
+  if st.session_state.messages[-1]["role"] != "assistant":
+      with st.chat_message("assistant"):
+          with st.spinner("考え中..."):
+              response = st.session_state.chat_engine.chat(prompt)
+              st.write(response.response)
+              message = {"role": "assistant", "content": response.response}
+              st.session_state.messages.append(message) # Add response to message history
+else:
+  st.text("きたよ")
